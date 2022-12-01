484b12bd06634280f63af2a827ff241a80595f40
JustNutron <kiraly.david222@gmail.com>
2022-12-01 11:41:13 +0100

    halál

--- backend.js
+++ backend.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql')
+var connection 
+
+function kapcsolat() {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pcosszerako_kd'
+  })
+  connection.connect()
+}
 
 app.use(express.static('kepek'))
 app.use(express.json())
 
 
-//------------------------     film lekérdezése
+//------------------------     processzor lekérdezése     ------------------------
 app.get('/processzor', (req, res) => {
-    const mysql = require('mysql')
-    const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'pcosszerako_kd'
-    })
-    
-    connection.connect()
-    
+
+
+
+    kapcsolat()
+      
     connection.query('SELECT * from processzor', (err, rows, fields) => {
       if (err) throw err
     
@@ -28,6 +35,46 @@ app.get('/processzor', (req, res) => {
   })
 
 
+//------------------------     alaplapok lekérdezése     ------------------------
+
+  app.get('/alaplap', (req, res) => {
+    
+    
+    kapcsolat()
+    
+    connection.query('SELECT * from alaplap', (err, rows, fields) => {
+      if (err) throw err
+    
+      res.send(rows)
+    })
+    
+    connection.end()
+  })
+
+
+  
+//------------------------     Pc Hírek lekérdezése     ------------------------
+
+  app.get('/pc_hirek', (req, res) => {
+    
+    
+    kapcsolat()
+    
+    connection.query('SELECT * from pc_hirek', (err, rows, fields) => {
+      if (err) throw err
+    
+      res.send(rows)
+    })
+    
+    connection.end()
+  })
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
