6bd6146ea56ba19d458b7df062495ba75c655a9c
JustNutron <kiraly.david222@gmail.com>
2023-01-24 07:32:36 +0100

    mégmindigszeretem

--- backend.js
+++ backend.js
@@ -69,6 +69,107 @@ app.get('/processzor', (req, res) => {
     connection.end()
   })
 
+//------------------------     Processzor hűtők lekérdezése     ------------------------
+
+app.get('/processzor_hutok', (req, res) => {
+    
+    
+  kapcsolat()
+  
+  connection.query('SELECT * from processzor_huto', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+//------------------------     Processzor hűtők lekérdezése     ------------------------
+
+app.get('/tapegyseg', (req, res) => {
+    
+    
+  kapcsolat()
+  
+  connection.query('SELECT * from tapegyseg', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+//------------------------     Videókártyák lekérdezése     ------------------------
+
+app.get('/videokartya', (req, res) => {
+    
+    
+  kapcsolat()
+  
+  connection.query('SELECT * from videokartya', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+//------------------------     Memóriák lekérdezése     ------------------------
+
+app.get('/memoria', (req, res) => {
+    
+    
+  kapcsolat()
+  
+  connection.query('SELECT * from memoria', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+
+//------------------------     Háttértárak lekérdezése     ------------------------
+
+
+app.get('/hattertar', (req, res) => {
+    
+    
+  kapcsolat()
+  
+  connection.query('SELECT * from hattertar', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+
+//------------------------     Gépház lekérdezése     ------------------------
+
+
+app.get('/gephaz', (req, res) => {
+    
+    
+  kapcsolat()
+  
+  connection.query('SELECT * from gephaz', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+
 
 
 
